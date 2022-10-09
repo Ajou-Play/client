c2226f2a9cdd8239ec1249bac0780a18ab91b84a
Coreight98 <maxcha98@naver.com>
2022-10-09 18:47:16 +0900

    style: 최소 width 추가

--- src/Component/TeamInfoContainer/TeamInfoContainer.tsx
+++ src/Component/TeamInfoContainer/TeamInfoContainer.tsx
@@ -1,7 +1,7 @@
 import { TeamInfoContainerProps } from './TeamInfoContainer.type';
 
 export const TeamInfoContainer = ({ children, teamName }: TeamInfoContainerProps) => (
-  <div className='h-[100vh] w-[272px] pt-4 px-4 box-border'>
+  <div className='h-[100vh] min-w-[272px] pt-4 px-4 box-border'>
     <div className='bg-main-color h-[50px] text-white p-4 box-border leading-4 font-extrabold text-base rounded-lg'>
       {teamName}
     </div>
