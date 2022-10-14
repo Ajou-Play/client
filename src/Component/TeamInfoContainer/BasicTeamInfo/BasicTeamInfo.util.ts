36c53c198bd8422c2bcfd5a4b86b54c115b1fa27
jin-Pro <dnjun2@ajou.ac.kr>
2022-10-14 18:40:17 +0900

    test: BasicTeamInfo test

--- src/Component/TeamInfoContainer/BasicTeamInfo/BasicTeamInfo.util.ts
+++ src/Component/TeamInfoContainer/BasicTeamInfo/BasicTeamInfo.util.ts
@@ -1,8 +1,13 @@
+export const GENERAL = 'General';
+export const GENERAL_SELECT_ICON_SRC = '/asset/GeneralChannelIcon.svg';
+export const GENERAL_UN_SELECT_ICON_SRC = '/asset/NoGeneralChannelIcon.svg';
+export const UN_GENERAL_SELECT_ICON_SRC = '/asset/ChannelIcon.svg';
+export const UN_GENERAL_UN_SELECT_ICON_SRC = '/asset/NoChannelIcon.svg';
 export const getChannelIconSrc = ({ name, select }: { name: string; select: boolean }) =>
-  name === 'General'
+  name === GENERAL
     ? select
-      ? '/asset/GeneralChannelIcon.svg'
-      : '/asset/NoGeneralChannelIcon.svg'
+      ? GENERAL_SELECT_ICON_SRC
+      : GENERAL_UN_SELECT_ICON_SRC
     : select
-    ? '/asset/ChannelIcon.svg'
-    : '/asset/NoChannelIcon.svg';
+    ? UN_GENERAL_SELECT_ICON_SRC
+    : UN_GENERAL_UN_SELECT_ICON_SRC;
