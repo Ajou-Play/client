5b8d6e447ee2d8c1394de7ecfec273c10a20e962
jin-Pro <dnjun2@ajou.ac.kr>
2022-10-14 16:14:40 +0900

    test: ChannelInfoContainer Util Unit Test By Jest

--- src/Component/ChannelInfoContainer/ChannelInfoContainer.util.ts
+++ src/Component/ChannelInfoContainer/ChannelInfoContainer.util.ts
@@ -1,6 +1,6 @@
 import { ChannelType } from '../TeamInfoContainer/BasicTeamInfo/BasicTeamInfo.type';
 
-const EMPTY_CHANNEL = {
+export const EMPTY_CHANNEL = {
   teamId: 0,
   channelId: -1,
   name: 'General',
@@ -13,7 +13,7 @@ type GetChannelInfo = {
 
 type FilterChannelSameId = GetChannelInfo;
 
-const filterChannelSameId = ({ channels, id }: FilterChannelSameId) =>
+export const filterChannelSameId = ({ channels, id }: FilterChannelSameId) =>
   channels.filter(({ channelId }) => channelId === id);
 
 export const getChannelInfo = ({ channels, id }: GetChannelInfo) => {
