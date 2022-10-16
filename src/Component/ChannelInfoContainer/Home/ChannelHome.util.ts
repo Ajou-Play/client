3162a4725539aba80c8c60146d45d8faf26b010e
jin-Pro <dnjun2@ajou.ac.kr>
2022-10-16 17:38:10 +0900

    fix: debounce util func memory leak solve

--- src/Component/ChannelInfoContainer/Home/ChannelHome.util.ts
+++ src/Component/ChannelInfoContainer/Home/ChannelHome.util.ts
@@ -1,10 +1,11 @@
 export const debounce = (func: Function, ms: number) => {
-  let timeout: ReturnType<typeof setTimeout>;
+  let timeout: ReturnType<typeof setTimeout> | null;
 
   return () => {
     if (timeout) clearTimeout(timeout);
 
     timeout = setTimeout(() => {
+      timeout = null;
       func();
     }, ms);
   };
