e3abb2a40f6bf1af1b3324f0c58e7d6100f35aee
jin-Pro <dnjun2@ajou.ac.kr>
2022-10-16 22:45:15 +0900

    fix: Socket Chat stompjs

--- src/Socket/Chat/index.ts
+++ src/Socket/Chat/index.ts
@@ -1,3 +1,1 @@
-export * from './joinChatRoom';
-export * from './leaveChatRoom';
 export * from './sendMessage';
--- src/Socket/Chat/leaveChatRoom.ts
+++
@@ -1,6 +0,0 @@
-import { emitEvent } from '../emitEvent';
-
-const eventName = 'leaveChatRoom';
-type LeaveChatRoom = { userId: string; chatRoom: string };
-
-export const leaveChatRoom = (body: LeaveChatRoom) => emitEvent({ eventName, body });
