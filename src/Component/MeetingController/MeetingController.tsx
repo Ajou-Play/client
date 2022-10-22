aca2303a667e5c7da0e49b8d88a3fea9d635998f
Coreight98 <maxcha98@naver.com>
2022-10-22 15:02:43 +0900

    refactor: meetingController 분리

--- src/Component/MeetingController/MeetingController.tsx
+++ src/Component/MeetingController/MeetingController.tsx
@@ -1,3 +1,5 @@
+import { motion } from 'framer-motion';
+
 import { CHARACTER_LEVEL } from './MeetingController.const';
 import { MeetingControllerProps } from './MeetingController.type';
 
@@ -8,105 +10,96 @@ const iconOnStyle =
 const iconOffStyle = 'w-[36px] h-[36px] flex justify-center items-center';
 const iconStyle = 'w-[20px] h-[20px] cursor-pointer';
 
-type MeetingProps = MeetingControllerProps & { toggleState: () => void };
-const MeetingOn = ({ userId, userLevel, toggleState }: MeetingProps) => {
-  const src = CHARACTER_LEVEL[userLevel];
+// type MeetingProps = MeetingControllerProps & { toggleState: () => void };
+
+const getUrlByMeetingState = (flag: boolean) => (flag ? 'mettingOn' : 'mettingOff');
+
+const MeetingToggle = () => {
+  const { state: meetingState, toggleState } = useToggle();
+
+  const MeetingToggleButton = () => (
+    <div
+      className={`w-[70px] h-[40px] rounded-[30px] border border-primary-lightOrange flex items-center cursor-pointer bg-primary-${
+        meetingState ? 'orange' : 'lightOrange'
+      } mettingToggleButton`}
+      onClick={toggleState}
+      aria-hidden
+      title='회의 나가기'
+    >
+      <motion.img
+        animate={{ x: meetingState ? 33 : 5 }}
+        src={`/asset/${getUrlByMeetingState(meetingState)}.svg`}
+        alt='meetingImg'
+        className='w-[30px] h-[30px]'
+      />
+    </div>
+  );
+
+  return {
+    state: meetingState,
+    component: MeetingToggleButton,
+  };
+};
+
+const MuteButton = () => {
   const { state: micState, toggleState: handleMicToggle } = useToggle();
-  const { state: windowState, toggleState: handleWindowToggle } = useToggle();
+
   return (
-    <div className='p-[16px] box-border w-[calc(100%-32px)] border border-primary-lightOrange rounded-xl bg-primary-lightOrange m-4'>
-      <div className='flex items-center'>
+    <div className='flex items-center muteButton'>
+      <div
+        className={`mr-4 ${micState ? ` ${iconOffStyle}` : `${iconOnStyle}`}`}
+        onClick={handleMicToggle}
+        aria-hidden
+        title={micState ? '음소거 해제' : '음소거'}
+      >
         <img
-          src={src}
-          alt='character'
-          className='w-[25px] h-[25px] rounded-lg mr-2'
+          src={micState ? '/asset/micOn.svg' : '/asset/micOff.svg'}
+          alt='mic'
+          className={iconStyle}
         />
-        <span className='text-xl'>{userId}</span>
-      </div>
-      <div className='flex justify-between'>
-        <div className='flex items-center '>
-          <div
-            className={`mr-4 ${micState ? ` ${iconOffStyle}` : `${iconOnStyle}`}`}
-            onClick={handleMicToggle}
-            aria-hidden
-            title={micState ? '음소거 해제' : '음소거'}
-          >
-            <img
-              src={micState ? '/asset/micOn.svg' : '/asset/micOff.svg'}
-              alt='mic'
-              className={iconStyle}
-            />
-          </div>
-          <div className={windowState ? iconOnStyle : iconOffStyle}>
-            <img
-              src={windowState ? '/asset/windowOff.svg' : '/asset/windowOn.svg'}
-              alt='mic'
-              className={iconStyle}
-              title={windowState ? '화면공유 중단' : '화면공유'}
-              onClick={handleWindowToggle}
-              aria-hidden
-            />
-          </div>
-        </div>
-        <div
-          className='w-[70px] h-[35px] rounded-2xl border border-primary-lightOrange flex items-center cursor-pointer bg-primary-orange justify-end'
-          onClick={toggleState}
-          aria-hidden
-          title='회의 나가기'
-        >
-          <img
-            src='/asset/mettingOn.svg'
-            alt='mettingOff'
-            className='w-[30px] h-[30px]'
-          />
-        </div>
       </div>
     </div>
   );
 };
 
-const MeetingOff = ({ userId, userLevel, toggleState }: MeetingProps) => {
-  const src = CHARACTER_LEVEL[userLevel];
+const ShareDisplayButton = () => {
+  const { state: windowState, toggleState: handleWindowToggle } = useToggle();
+
   return (
-    <div className='flex justify-between p-[16px] box-border items-center w-[100%]'>
-      <div className='flex items-center'>
+    <div className='flex justify-between shareButton'>
+      <div className={windowState ? iconOnStyle : iconOffStyle}>
         <img
-          src={src}
-          alt='character'
-          className='w-[30px] h-[30px] rounded-lg mr-2'
-        />
-        <span className='text-xl'>{userId}</span>
-      </div>
-      <div
-        className='w-[70px] h-[35px] rounded-2xl border border-primary-lightOrange flex items-center cursor-pointer bg-primary-lightOrange '
-        onClick={toggleState}
-        aria-hidden
-        title='회의 참여하기'
-      >
-        <img
-          src='/asset/mettingOff.svg'
-          alt='mettingOff'
-          className='w-[30px] h-[30px]'
+          src={windowState ? '/asset/windowOff.svg' : '/asset/windowOn.svg'}
+          alt='mic'
+          className={iconStyle}
+          title={windowState ? '화면공유 중단' : '화면공유'}
+          onClick={handleWindowToggle}
+          aria-hidden
         />
       </div>
     </div>
   );
 };
-export const MeetingController = (props: MeetingControllerProps) => {
-  const { state: meetingState, toggleState } = useToggle();
+
+export const MeetingController = ({ userId, userLevel }: MeetingControllerProps) => {
+  const src = CHARACTER_LEVEL[userLevel];
+  const { component: MeetingToggleButton, state: meetingState } = MeetingToggle();
+
   return (
-    <div className='border-t-grey-background border-t-2'>
-      {meetingState ? (
-        <MeetingOn
-          {...props}
-          toggleState={toggleState}
-        />
-      ) : (
-        <MeetingOff
-          {...props}
-          toggleState={toggleState}
-        />
-      )}
+    <div className='p-[10px] box-border mt-[10px] border-t-2'>
+      <div className={`border-t-grey-background  ${meetingState ? 'meetingOn' : 'meetingOff'}`}>
+        <div className='flex items-center title'>
+          <img
+            src={src}
+            alt='character'
+            className='w-[30px] h-[30px] rounded-lg mr-2'
+          />
+          <span className='text-xl'>{userId}</span>
+        </div>
+        {meetingState && <MuteButton />}
+        {meetingState && <ShareDisplayButton />}
+        <MeetingToggleButton />
+      </div>
     </div>
   );
 };
