03bf7b700d4305085eb41339021c6c69c955b837
Coreight98 <maxcha98@naver.com>
2022-10-22 21:20:10 +0900

    feat: add error style and error message in input tag

--- src/Component/LoginInput/LoginInput.tsx
+++ src/Component/LoginInput/LoginInput.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
-import { LoginInputWrapper, LoginInputTitle, LoginInputStyle } from './LoginInput.style';
+import { LoginInputWrapper, LoginInputTitle } from './LoginInput.style';
 
 import type { InputType } from '@Component/.';
 
 export const LoginInput = React.forwardRef<HTMLInputElement, InputType>(
-  ({ type, titleContent, icon, placeholder, ...rest }, ref) => (
+  ({ type, titleContent, icon, placeholder, error, errorMessage, ...rest }, ref) => (
     <>
       <p className={LoginInputTitle}>{titleContent}</p>
       <div className={LoginInputWrapper}>
         <img
+          className='absolute left-0 top-[50%] translate-y-[-50%]'
           src={icon}
           alt='아이콘'
         />
@@ -18,9 +19,12 @@ export const LoginInput = React.forwardRef<HTMLInputElement, InputType>(
           ref={ref}
           {...rest}
           placeholder={placeholder}
-          className={LoginInputStyle}
+          className={`w-screen font-extralight h-[3rem] pl-[2rem] mb-[2px] ${
+            error ? 'border-system-error' : 'border-grey-line'
+          } border-b-[2px] focus:outline-none focus:border-grey-label`}
         />
       </div>
+      {error && <p className='text-system-error'>{errorMessage}</p>}
     </>
   ),
 );
