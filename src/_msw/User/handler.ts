301c4f1fb073f22dddad3066798636d222ee5718
Coreight98 <maxcha98@naver.com>
2022-10-23 19:38:31 +0900

    feat: add msw login and register

--- src/_msw/User/handler.ts
+++ src/_msw/User/handler.ts
@@ -1,8 +1,38 @@
 import { Props } from '../type';
 
-export const mockLocalLogin: Props = (req, res, ctx) => res(ctx.json(true));
+export const mockLocalLogin: Props = (req, res, ctx) => {
+  const {
+    params: { email, password },
+  } = req;
+  res(
+    ctx.json({
+      message: '로그인 성공',
+      status: 200,
+      data: {
+        userId: 1,
+        accessToken: 'fdafda',
+        refreshToken: 'fdafda',
+      },
+    }),
+  );
+};
 export const mockSocialLogin: Props = (req, res, ctx) => res(ctx.json(true));
-export const mockRegister: Props = (req, res, ctx) => res(ctx.json(true));
+export const mockRegister: Props = (req, res, ctx) => {
+  const {
+    params: { email, name, password },
+  } = req;
+  return res(
+    ctx.json({
+      message: '회원가입 성공',
+      status: 201,
+      data: {
+        userId: 1,
+        name,
+        email,
+      },
+    }),
+  );
+};
 export const mockLogOut: Props = (req, res, ctx) => res(ctx.json(true));
 export const mockGetToken: Props = (req, res, ctx) => res(ctx.json(true));
 export const mockGetUserInfo: Props = (req, res, ctx) => res(ctx.json(true));
