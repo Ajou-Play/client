8c64563a59024a6ae98c8385ae3a11d47a976f47
jin-Pro <dnjun2@ajou.ac.kr>
2022-11-09 19:48:52 +0900

    delete: duplicate meetingComponent

--- src/Component/MeetingController/MeetingController.tsx
+++ src/Component/MeetingController/MeetingController.tsx
@@ -7,38 +7,6 @@ const iconOnStyle =
   'flex justify-center items-center w-[36px] h-[36px] rounded-lg bg-primary-point-black';
 const iconOffStyle = 'w-[36px] h-[36px] flex justify-center items-center';
 const iconStyle = 'w-[20px] h-[20px] cursor-pointer';
-
-// type MeetingProps = MeetingControllerProps & { toggleState: () => void };
-
-// const getUrlByMeetingState = (flag: boolean) => (flag ? 'mettingOn' : 'mettingOff');
-
-// const MeetingToggle = () => {
-//   const { state: meetingState, toggleState } = useToggle();
-
-//   const MeetingToggleButton = () => (
-//     <div
-//       className={`w-[70px] h-[40px] rounded-[30px] border border-primary-lightOrange flex items-center cursor-pointer bg-primary-${
-//         meetingState ? 'orange' : 'lightOrange'
-//       } mettingToggleButton`}
-//       onClick={toggleState}
-//       aria-hidden
-//       title='회의 나가기'
-//     >
-//       <motion.img
-//         animate={{ x: meetingState ? 33 : 5 }}
-//         src={`/asset/${getUrlByMeetingState(meetingState)}.svg`}
-//         alt='meetingImg'
-//         className='w-[30px] h-[30px]'
-//       />
-//     </div>
-//   );
-
-//   return {
-//     state: meetingState,
-//     component: MeetingToggleButton,
-//   };
-// };
-
 const MuteButton = () => {
   const { micState, handleMicToggle } = useMicState();
 
