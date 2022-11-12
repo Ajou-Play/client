7651594426fb93e0a33bbb801d430ee39c600056
Coreight98 <maxcha98@naver.com>
2022-11-13 01:38:50 +0900

    chore: 모달 export init

--- src/Component/Modal/index.ts
+++ src/Component/Modal/index.ts
@@ -1,1 +1,6 @@
-export * from './Modal';
+export * from './ExitTeamModal/ExitTeamModal';
+export * from './AddTeamModal/AddTeamModal';
+export * from './AddChannelModal/AddChannelModal';
+export * from './SelectSharedDisplayModal/SelectSharedDisplayModal';
+export * from './SetTeamModal/SetTeamModal';
+export * from './SetPersonalModal/SetPersonalModal';
