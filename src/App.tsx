ede1329ae41b1e3d987f29e6d12397942414a479
Coreight98 <maxcha98@naver.com>
2022-11-23 00:53:44 +0900

    fix: import error

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Route, Routes } from 'react-router-dom';
 
 import './App.css';
-import { WordView, MainView, PresentationView } from './Component';
+import { MainView } from './Component';
 
 import { LoginPage, MainPage, PrivatePage, RegisterPage } from '@Page/.';
 
